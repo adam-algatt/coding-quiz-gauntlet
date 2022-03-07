84aba3b26203bce047fbc87fba7be31598e6c8a1
adam-algatt <adam.algatt@gmail.com>
2022-03-06 18:13:18 -0800

    added questions arr

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,6 +3,24 @@ var btnEl = document.getElementById('quiz-btn');
 var timer = document.getElementById('timer');
 var nav = document.getElementById('header');
 
+var quizQuestions = [
+{"question": "javascript is a ______ oriented programming language.",
+"correct": "object",
+"wrong": ["program", "function", "prayer"] 
+},
+{"question": "javascript is a ______ typed language.",
+"correct": "loosely",
+"false": ["false", "happy", "quickly"] 
+},
+{"question": "[{'name:' 'bob'}]; is an example of a _________ object.",
+"correct": "nested",
+"false": ["bedded", "sleepy", "asynchronous"] 
+},
+
+//function 
+
+];
+//starts countdown timer once start quiz btn is clicked
 function Countdown(seconds) {
     let counter = seconds;
 
